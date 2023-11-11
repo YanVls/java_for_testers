cb3ec9d8f0102d696fa0ee2ab00ede934c70e3ea
YanVls <yan.vlasenko@mail.ru>
2023-11-11 13:21:12 +0400

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -0,0 +1,14 @@
+package ru.stqa.geometry;
+
+import ru.stqa.geometry.figures.Rectangle;
+import ru.stqa.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquare(6.0);
+
+        Rectangle.printRectangleArea(3.0, 5.0);
+        Rectangle.printRectangleArea(8.0, 12.0);
+    }
+
+}
