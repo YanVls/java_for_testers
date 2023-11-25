0d51c2efde4618c41c61a167f36c93168cc0a91e
YanVls <yan.vlasenko@mail.ru>
2023-11-26 01:21:24 +0400

    Revert "Revert "Написание тестов выводящих исключение.""
    This reverts commit 3aaf9d1a052d8b79463c116dd22e5c8271307767.

--- sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -7,13 +7,27 @@ public class SquareTests {
 
     @Test
     void canCalculateArea() {
-        var s = new Square(5.0);
+        var s = new Square(4.0);
         double actual = s.area();
-        Assertions.assertEquals(25, actual);
+        // Assertions.assertEquals(25, actual);
+        if (actual != 25.0) {
+            throw new AssertionError(String.format("Expected format %f , but was %f", 25.0, actual));
+        }
     }
 
     @Test
     void canCalculatePerimeter() {
-        Assertions.assertEquals(20.0,new Square(5.0).perimeter());
+        Assertions.assertEquals(20.0, new Square(5.0).perimeter());
+    }
+
+    @Test
+    void cannotCreateSquareWithNegativeSide() {
+        try {
+            new Square(-5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+
     }
 }
