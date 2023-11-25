af1f09131fa39462f95bf50d4ce41fcd5b261ae5
YanVls <yan.vlasenko@mail.ru>
2023-11-26 01:13:07 +0400

    Написание тестов выводящих исключение.

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,12 @@
 package ru.stqa.geometry.figures;
 
-public class Rectangle {
+public record Rectangle(double a, double b) {
+    public Rectangle {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Side cannot be negative");
+        }
+    }
+
     public static void printRectangleArea(double a, double b) {
         String text = String.format("Площадь прямоугольника со стороной %f и %f = %f", a, b, rectangleArea(a, b));
         System.out.println(text);
